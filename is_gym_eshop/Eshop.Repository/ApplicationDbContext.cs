87052a6c124d621b6ee11879bf89886c859ceab7
marko <markozoreski@gmail.com>
2024-01-31 01:06:06 +0100

    configured compose file. just need to fix volume creation

--- is_gym_eshop/Eshop.Repository/ApplicationDbContext.cs
+++ is_gym_eshop/Eshop.Repository/ApplicationDbContext.cs
@@ -3,6 +3,9 @@ using Eshop.Domain.Identity;
 using Eshop.Domain.Relations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace Eshop.Repository
@@ -10,9 +13,21 @@ namespace Eshop.Repository
     public class ApplicationDbContext : IdentityDbContext<EshopUser>
     {
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
             : base(options)
         {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                    if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public virtual DbSet<Product> Products { get; set; }
@@ -20,11 +35,13 @@ namespace Eshop.Repository
         public virtual DbSet<ProductInShoppingCart> ProductInShoppingCarts { get; set; }
         public virtual DbSet<EmailMessage> EmailMessages { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShopD;Trusted_Connection=True;MultipleActiveResultSets=true", b => b.MigrationsAssembly("Eshop.Web"));
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlServer("Data Source=DESKTOP-4QCTE4I;Initial Catalog=GymDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;", b => b.MigrationsAssembly("Eshop.Web"));
+        //    // database at home
+        //    //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShopD;Trusted_Connection=True;MultipleActiveResultSets=true", b => b.MigrationsAssembly("Eshop.Web"));
+        //}
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
