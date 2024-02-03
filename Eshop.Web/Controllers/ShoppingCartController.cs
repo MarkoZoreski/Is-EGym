c8857a9e29c75fd871d4cb7b789bfb916eea73fb
marko <mz@norliga.com>
2024-02-03 22:14:31 +0100

    test1

---
+++ Eshop.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,57 @@
+﻿using Eshop.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Eshop.Web.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return View(this._shoppingCartService.getShoppingCartInfo(userId));
+        }
+
+        public IActionResult DeleteFromShoppingCart(Guid id)
+        {
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._shoppingCartService.deleteProductFromSoppingCart(userId, id);
+            
+            if(result)
+            {
+                return RedirectToAction("Index", "ShoppingCart");
+            }
+            else
+            {
+                return RedirectToAction("Index", "ShoppingCart");
+            }
+        }
+
+      
+        public IActionResult Success()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._shoppingCartService.order(userId);
+            ViewData["message"] = result;
+            return View();
+        }
+
+    }
+}
